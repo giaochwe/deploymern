301c26cd9532b55173666bedb73181c0efa0fde0
riggina <oceaniariggina@gmail.com>
2023-12-05 01:42:24 +0800

    update

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGO)
 const app = express();
 app.use(cors(
     {
-        origin: ["http://localhost:5173/"],
+        origin: ["https://deploymern-fe-riggina.vercel.app/"],
         methods: ["POST", "GET"],
         credentials: true
     }
