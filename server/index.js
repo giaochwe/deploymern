273595c3892bbc3b4099af59568d47f1f2fd8586
riggina <oceaniariggina@gmail.com>
2023-12-05 01:27:19 +0800

    update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
-import path from 'path';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -16,8 +15,6 @@ mongoose.connect(process.env.MONGO)
     .catch((err) => {
     console.log(err);
     });
-
-    const __dirname = path.resolve();
 
 const app = express();
 
@@ -30,16 +27,12 @@ app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
 
-
+app.get("/", (req, res) => {
+    res.json("Hello");
+})
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
-
-app.use(express.static(path.join(__dirname, '/fe/dist')));
-
-app.get('*',(req, res) => {
-    res.sendFile(path.join(__dirname, 'fe', 'dist', 'index.html'));
-})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
